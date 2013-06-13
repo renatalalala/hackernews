a5c680837c4fcdba4d6ead82d4a948a210fca404
Apprentice <apprentice@dbc15.local>
2013-06-12 17:55:25 -0700

    Got upvotes and downvotes to work

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -18,8 +18,18 @@ get '/post/:id' do |id|
   erb :post_detailed
 end
 
-
 get '/user/:id' do |id|
   @user = User.find(id)
   erb :user_profile
+end
+
+post '/postvote' do
+  p params
+
+  user = User.find(params[:user_id])
+  post = Post.find(params[:post_id])
+  vote_value = params[:value].to_i
+
+  p user, post, vote_value
+  Postvote.create(user: user, post: post, vote_value: vote_value)
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.upvote').on('click', function() {
+    that = $(this);
+    $.ajax({url: '/postvote',
+            type: 'POST',
+            data: that.attr('name')+"&value=1"
+    });
+  });
+
+  $('.downvote').on('click', function() {
+    that = $(this);
+    $.ajax({url: '/postvote',
+            type: 'POST',
+            data: that.attr('name')+"&value=-1"
+    });
+  });
+
 });
